84706e7ab4e20686dfc2b46ccb9d218b3c0c2270
Dmitriy Krainiy <dmitriy.krainiy@spryker.com>
2025-10-20 15:59:09 +0200

    AE-207: Updated paths after migration to monorepository

--- tests/SprykerShopTest/Yves/ErrorPage/Handler/RedirectExceptionHandlerTest.php
+++ tests/SprykerShopTest/Yves/ErrorPage/Handler/RedirectExceptionHandlerTest.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace SprykerShopTest\Yves\ErrorPage\ExceptionHandler;
+namespace SprykerShopTest\Yves\ErrorPage\Handler;
 
 use Codeception\Test\Unit;
 use Exception;
